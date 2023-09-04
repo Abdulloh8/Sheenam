c3d412a84cd25b66bac70d0280418161e8a10f8a
Your Name <you@example.com>
2023-09-04 22:31:37 +0500

    ShouldThrowValidationExceptionOnAddIfGuestIsNullAndLogitAsync

--- Sheenam.Api.Tests.Unit/Services/Foundations/Guests/GuestServiceTests.Validations.Add.cs
+++ Sheenam.Api.Tests.Unit/Services/Foundations/Guests/GuestServiceTests.Validations.Add.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Sheenam.Api.Models.Foundations.Guests;
+using Sheenam.Api.Models.Foundations.Guests.Exceptions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,10 +14,21 @@ namespace Sheenam.Api.Tests.Unit.Services.Foundations.Guests
         public async Task ShouldThrowValidationExceptionOnAddIfGuestIsNullAndLogitAsync()
         {
             //given
+            Guest nullGuest = null;
+            var nullGuestException = new NullGuestException();
+
+            var expectedGuestValidationException =
+                new GuestValidationException(nullGuestException);
 
             //when
 
+            ValueTask<Guest> addGuestTask = 
+                this.guestService.AddGuestAsync(nullGuest);
+
             //then
+
+            await Assert.ThrowsAsync<GuestValidationException>(() =>
+            addGuestTask.AsTask());
         }
     }
 }
